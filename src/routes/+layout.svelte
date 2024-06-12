bb7691eecc5a1cfdcb4da4740d94095a06798db0
Anastasia <anastasia.trombly@gmail.com>
2024-06-11 21:58:41 -0400

    basic skeleton updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,20 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import Sidebar from './sidebar.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<slot />
+<div class="flex flex-col gap-2 border border-black m-2">
+	<div class="flex flex-row gap-2 border border-red"> <h1 class="h1">Hatch the Egg</h1> </div>
+	<div class="flex flex-row gap-2 border border-yellow">
+		<Sidebar />
+		<div class="flex flex-col gap-2 border-red">
+			<slot />
+		</div>
+	</div>
+
+
+</div>
+
+
---
+++ src/routes/sidebar.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col gap-2 border-black" style="max-width: 30vw;">
+    <h2 class="h2">Sidebar</h2>
+    And some more sidebar content
+</div>

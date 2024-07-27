7d2775fb88d866dab357c8e64e56b0300a7fac09
Anastasia <anastasia.trombly@gmail.com>
2024-07-27 12:30:16 -0400

    birds flap

--- src/lib/components/Creature.svelte
+++ src/lib/components/Creature.svelte
@@ -10,15 +10,27 @@
 
     let canvas_element: HTMLCanvasElement   
 
-    onMount(() => {
+    function LoadBird(x: number, y: number) {
         const image = new Image()
         image.onload = () => {
-            canvas_element.getContext("2d")?.drawImage(image, 16, 48, 16, 16, 0, 0, 16, 16)
+            canvas_element.getContext("2d")?.clearRect(0, 0, canvas_element.width, canvas_element.height)
+            canvas_element.getContext("2d")?.drawImage(image, x, y, 16, 16, 0, 0, 16, 16)
         }
         image.src=image_src
+    }
+
+    onMount(() => {
+        LoadBird(16,16)
+        FlapWings()
     })
 
+    function FlapWings() {
+        LoadBird(32,16)
+        setTimeout(() => {LoadBird(16,16)}, 130 + (Math.random() * 100))
+    }
+
     function ClickCreature() {
+        console.log("click")
         if (clicker) {
             count += 1
         }
@@ -26,7 +38,7 @@
 
 </script>
 
-<canvas id="creature" class={(clicker) ? "cursor-pointer" : ""} width="16" height="16" bind:this={canvas_element} on:click={ClickCreature}>Creature</canvas>
+<canvas id="creature" class={(clicker) ? "cursor-pointer" : ""} width="16" height="16" bind:this={canvas_element} on:click={ClickCreature} on:mouseenter={FlapWings}>Creature</canvas>
 
 <style>
 
